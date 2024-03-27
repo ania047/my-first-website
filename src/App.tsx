ffe2b99ec9668fe831476857702326e690b73d38
Razy112 <raz.drg113@gmail.com>
2024-03-27 17:18:55 +0200

    add name

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Ioni</h1>
-      <h1>Cristi</h1>
+      <h1>Razvan</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
